ed2897b53d1659b5c910cefb152290296c6212fd
Rita Tang <doggymint@gmail.com>
2023-09-11 21:09:35 -0400

    Implement character counter for tweet form with jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// Display the remaining characters for the new tweet form
+// Render in RED with the invalid class when the textarea has more than 140 characters
+$("document").ready(function () {
+  $("textarea").on("keyup", function () {
+    let remainingChar = 140 - this.value.length;
+    const counter = $(this).parent().find(".counter");
+
+    counter.text(remainingChar);
+    // Add the invalid class to the counter output when the remaining character is less than 0
+    remainingChar < 0
+      ? counter.addClass("invalid")
+      : counter.removeClass("invalid");
+  });
+});
