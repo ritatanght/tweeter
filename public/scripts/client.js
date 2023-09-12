20703fce06faf3bf56322549902b595fee127781
Rita Tang <doggymint@gmail.com>
2023-09-12 17:08:24 -0400

    Add createTweetElement and renderTweets, remove dummy tweet from index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// sample tweets data
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+/**
+ * Takes a tweet object and generate a new HTML element
+ * @param {object} tweetObject
+ * @returns jquery element
+ */
+const createTweetElement = (tweetObject) => {
+  const { user, content, created_at } = tweetObject;
+  const $tweet = $(`<article class="tweet">
+  <header>
+            <div>
+              <img src="${user.avatars}" alt="avatar" />
+              <p>${user.name}</p>
+            </div>
+            <p class="handle">${user.handle}</p>
+          </header>
+          <p>
+            ${content.text}
+          </p>
+          <footer>
+            <p class="create-time">${created_at} ago</p>
+            <div class="tweet-icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-repeat"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+  </article>`);
+  return $tweet;
+};
+
+/**
+ * Takes in an array of tweet objects and append each one to the #tweets-container
+ * @param {array} tweets
+ * @returns undefined
+ */
+const renderTweets = (tweets) => {
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $("#tweets-container").append($tweet);
+  }
+};
+
+$(document).ready(() => {
+  renderTweets(data);
+});
