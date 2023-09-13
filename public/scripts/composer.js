574ff36331e204bed0b730e8c1fd834eb84ed2d4
Rita Tang <doggymint@gmail.com>
2023-09-13 16:23:33 -0400

    Add scroll to top button

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -9,4 +9,25 @@ $(document).ready(() => {
     });
   });
 
+  /**
+   * (Stretch)
+   * Make the 'Go To Top' button appear when the user starts to scroll and the nav button disappear
+   */
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 120) {
+      $(".compose-btn").hide();
+      $(".top-btn").show();
+    } else {
+      $(".compose-btn").show();
+      $(".top-btn").hide();
+    }
+  });
+
+  // When clicked, the page should scroll to the top and the form should slide down with the textarea enabled.
+  $(".top-btn").click(() => {
+    $(window).scrollTop(0);
+    $(".new-tweet").slideToggle("fast", function () {
+      $(this).find("#tweet-text").focus();
+    });
+  });
 });
