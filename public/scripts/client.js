5a788150f0ee62f744db7b795a4e11c2f5c81fdf
Rita Tang <doggymint@gmail.com>
2023-09-12 21:03:08 -0400

    Add form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// sample tweets data
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
 
 /**
  * Takes a tweet object and generate a new HTML element
@@ -78,12 +53,22 @@ const renderTweets = (tweets) => {
 // jQuery
 $(document).ready(() => {
   // form data submission
-  $("form").submit(function (event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-    const [key, val] = $(this).serialize().split("=");
-    const data = { [key]: val };
+    // Knowing that our form only has 1 text field, we can immediately extract the input
+    let text = $(this).serialize().replace("text=", "");
+    // decode the query string
+    text = decodeURIComponent(text);
 
-    $.post("/tweets", data, () => {
+    // disallow tweet content to be empty or exceeds 140 characters.
+    if (!text) {
+      return alert("You cannot tweet empty content");
+    } else if (text.length > 140) {
+      console.log(text.length);
+      return alert("The tweet content is too long");
+    }
+
+    $.post("/tweets", { text }, () => {
       console.log("Posted");
     });
   });
